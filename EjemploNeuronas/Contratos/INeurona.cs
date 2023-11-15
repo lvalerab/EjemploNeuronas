2246a6366feec4edac090777d2652c8a32367355
Luis Fernando Valera Bernal <lvalerab@gmail.com>
2023-11-15 07:21:55 +0100

    Actualizar INeurona.cs
    Se cambia el contrato para que se especifique el tipo de entrada, el tipo de salida y el tipo del kernel

--- EjemploNeuronas/Contratos/INeurona.cs
+++ EjemploNeuronas/Contratos/INeurona.cs
@@ -5,10 +5,11 @@ using System.Text;
 namespace EjemploNeuronas.Contratos
 {
     public delegate double FuncionActivacion(double entrada);
-    public interface INeurona<T>
-    {
-        public T Kernel { get; set; }
-        public T Entradas { get; set; }
+    public interface INeurona<E,K,S>
+    { 
+        public S Salidas {get; set; }
+        public K Kernel { get; set; }
+        public E Entradas { get; set; }
         public double Salida { get; set; }
         public double? Sesgo { get; set; }
         public void CalcularSalida();
